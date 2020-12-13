d2c47f0c0086a92c1a1a16a2ec1a1240e764d08d
savducharm <74573090+savducharm@users.noreply.github.com>
2020-12-13 10:20:59 -0800

    Fix hamburger display

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log("Hi there! Welcome to my protfolio :)")
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
