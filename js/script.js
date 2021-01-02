85d8dac730a36341b27ba804cce3e1ba620c7e65
savducharm <74573090+savducharm@users.noreply.github.com>
2021-01-01 21:07:33 -0800

    Add linters and cross-device fixes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-console.log("Hi there! Welcome to my protfolio :)")
+console.log('Hi there! Welcome to my protfolio :)')
 
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
